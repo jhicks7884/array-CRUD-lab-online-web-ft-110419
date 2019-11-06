05d6c43f71706345dd8e9ee95d95268a1b41b842
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-06 17:32:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+     array.pop
 end
 
 def remove_element_from_start_of_array(array)
@@ -31,5 +31,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
+
 
 end
