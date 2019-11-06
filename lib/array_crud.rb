ddbd0a30b6314c766f4b8c2076141cdba88b68f1
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-06 17:29:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-my_pets = [cats, dogs, birds, reptiles]
+ ["cats", "dogs", "birds", "reptiles"]
 end
 
 def add_element_to_end_of_array(array, element)
